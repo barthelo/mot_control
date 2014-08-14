390f4d9270fe4f5f251362edc2a21ba71fad6cbf
barthelo <nutzer.ben@gmail.com>
2014-08-15 01:09:09 +0200

    Created Files PIController source and header files were created and also an entry in the makefile was added.

---
+++ inc/PIControl.h
@@ -0,0 +1,18 @@
+/**
+ * *************************************************************************
+ * @file     PIControl.h
+ * @author   Eugen Bartel
+ * @date     August-14-2014
+ * @brief    Contains declarations and defines for PIController usage
+ ******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __PIControl_H
+#define __PIControl_H
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx.h"
+/* Private define ------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+#endif /* __PIControl_H */

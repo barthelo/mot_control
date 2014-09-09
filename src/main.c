afceae14767967f0ece8d0b8fd51f7c3b0349a19
barthelo <nutzer.ben@gmail.com>
2014-09-09 22:14:42 +0200

    New speed controller settings

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ void BTN_vInit(void);
 PIC CurrentController;
 PIC SpeedController;
 
-float RPM; 
+float RPM;
 
 void TIM3_IRQHandler(void)
 {
@@ -22,15 +22,15 @@ void TIM3_IRQHandler(void)
   float dutycycle;
   float current_set;
   if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
-  {    
+  {   
     GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
     current_set=PIC_fCalcCommand(&SpeedController,RPM,speed);
     dutycycle=PIC_fCalcCommand(&CurrentController,current_set,current);
     dutycycle=dutycycle*100;
     if(dutycycle<0)
-        FQC_vSetDutyCycleBackward(dutycycle*-1.0);
+	FQC_vSetDutyCycleBackward(dutycycle*-1.0);
     else
-        FQC_vSetDutyCycleForward(dutycycle);
+	FQC_vSetDutyCycleForward(dutycycle);
 
     TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
   }
@@ -45,17 +45,15 @@ int main(void)
   FQC_vInit();
   BTN_vInit();
   ADC_vInit();
-  
+
   /* Start ADC cenversion (Position is variable in source code)*/
   ADC_SoftwareStartConv(ADC1);
-  
-  
+
   PIC_vConstructor(&CurrentController,0.90295848990433,0.06169815417594,1,-1,1,1/100.0);
   uint16_t adc_value;
   float voltage_value;
   float current_value;
-  
-  
+    
   float rpm=0;
   float signal=0;
   
@@ -63,14 +61,12 @@ int main(void)
   {
     if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0))
     {
-//       FQC_vSetDutyCycleBackward(20);
       RPM=10000;
       GPIO_SetBits(GPIOD, GPIO_Pin_15);
       signal=1;
     }
     else
     {
-      /*FQC_vSetDutyCycleForward(20);*/
       RPM=20000;
       GPIO_ResetBits(GPIOD, GPIO_Pin_15);
       signal=0;
@@ -79,15 +75,15 @@ int main(void)
     voltage_value=ADC_fGetVoltage(adc_value);
     current_value=ADC_fGetCurrent(voltage_value);
     rpm=ENC_fGetRPM();
-    /*USART_vSendFloatAsString(signal);*/
-    /*USART_vSendFloatAsString(rpm);*/
+    USART_vSendFloatAsString(signal);
+    USART_vSendFloatAsString(rpm);
     USART_vSendFloatAsString(current_value);
   }
 }
 
 void BTN_vInit(void)
 {
-  /* Declare structures*/
+  /* Structure declaration*/
   GPIO_InitTypeDef GPIO_InitStructure;
   
   /* Activate clock */
